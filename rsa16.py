db596104be35bebbebd8433360aec989bd8e38a9
Bader alotaibi <91114465+BDR-Pro@users.noreply.github.com>
2023-10-04 14:05:11 +0300

    Add files via upload

--- rsa16.py
+++ rsa16.py
@@ -11,30 +11,32 @@ def generate_keypair(bits):
     e = 65537  # Commonly used public exponent
     d = sympy.mod_inverse(e, phi)
 
-    public_key = (n, e)  # Store (n, e) as the public key
-    private_key = (n, d)  # Store (n, d) as the private key
+    public_key = (n)  # Store (n, e) as the public key
+    private_key = (d)  # Store (n, d) as the private key
 
     return public_key, private_key
 
 def encrypt(public_key, plaintext):
-    n, e = public_key
+    n= public_key
+    e=65537
     encrypted_msg = pow(plaintext, e, n)
     return encrypted_msg
 
 def decrypt(private_key, ciphertext):
-    n, d = private_key
+    d = private_key
+    n=public_key
     decrypted_msg = pow(ciphertext, d, n)
     return decrypted_msg
 
 # Example usage
 public_key, private_key = generate_keypair(16)
-message = 29201
+message = 123456789
 ciphertext = encrypt(public_key, message)
 decrypted_message = decrypt(private_key, ciphertext)
-nbin=bin(public_key[0]).replace("0b", "")
+nbin=bin(public_key).replace("0b", "")
 print("Original Message:", message)
-print("Public Key (n, e):", public_key)
-print("Private Key (n, d):", private_key)
+print("Public Key n:", public_key)
+print("Private Key d:", private_key)
 print("Ciphertext:", ciphertext)
 print("Decrypted Message:", decrypted_message)
 print("n of public key in binary:",nbin)
